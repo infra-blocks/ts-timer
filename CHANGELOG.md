cdccfb016573207120f8d765e0c257e175e9cfb0
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-11-05 05:49:51 -0500

    first implementation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,10 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.1.0] - 2024-04-18
+## [0.1.0] - 2025-11-05
 
 ### Added
 
-- Initial release of the package!
+- Initial release of the package! Exposing a timer that uses `setTimeout` with an event emitter API
+and that can be restarted!
 
 [0.1.0]: https://github.com/infra-blocks/ts-timer/releases/tag/v0.1.0
--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -1,9 +1,85 @@
-import { expect } from "@infra-blocks/test";
+import { expect, sinon } from "@infra-blocks/test";
+import { timer } from "../../src/index.js";
 
-describe("index", () => {
-  describe("dummy test", () => {
-    it("should return the right stuff", () => {
-      expect(true).to.be.true;
-    });
+declare module "mocha" {
+  interface Context {
+    clock: sinon.SinonFakeTimers;
+  }
+}
+
+describe("timer", () => {
+  before(function () {
+    this.clock = sinon.useFakeTimers();
+  });
+
+  after(function () {
+    this.clock.restore();
+  });
+
+  it("should dispatch to the callback at the proper time ", function () {
+    const handler = sinon.fake();
+    timer(100).on("timeout", handler).start();
+    this.clock.tick(99);
+    expect(handler).to.not.have.been.called;
+    this.clock.tick(1);
+    expect(handler).to.have.been.calledOnce;
+  });
+  it("should do handle double starts as if it had been called only once", function () {
+    const handler = sinon.fake();
+    const t = timer(100).on("timeout", handler).start();
+    this.clock.tick(90);
+    expect(handler).to.not.have.been.called;
+    t.start();
+    this.clock.tick(10);
+    expect(handler).to.have.been.calledOnce;
+    this.clock.tick(100);
+    // Still called only once.
+    expect(handler).to.have.been.calledOnce;
+  });
+  it("should treat cancellation as a no-op when not started", () => {
+    // It's not throwing.
+    timer(100).cancel();
+  });
+  it("should prevent the timeout callback when cancelled", function () {
+    const handler = sinon.fake();
+    const t = timer(100).on("timeout", handler).start();
+    this.clock.tick(50);
+    expect(handler).to.not.have.been.called;
+    t.cancel();
+    this.clock.tick(50);
+    expect(handler).to.not.have.been.called;
+  });
+  it("should be able to be restarted after completion", function () {
+    const handler = sinon.fake();
+    const t = timer(100).on("timeout", handler).start();
+    this.clock.tick(100);
+    expect(handler).to.have.been.calledOnce;
+    t.restart();
+    this.clock.tick(100);
+    expect(handler).to.have.been.calledTwice;
+  });
+  it("should be able to be restarted mid-timer", function () {
+    const handler = sinon.fake();
+    const t = timer(100).on("timeout", handler).start();
+    this.clock.tick(50);
+    expect(handler).to.not.have.been.called;
+    t.restart();
+    this.clock.tick(50);
+    expect(handler).to.not.have.been.called;
+    this.clock.tick(50);
+    expect(handler).to.have.been.calledOnce;
+  });
+  it("should be able to be restarted after cancellation", function () {
+    const handler = sinon.fake();
+    const t = timer(100).on("timeout", handler).start();
+    this.clock.tick(50);
+    expect(handler).to.not.have.been.called;
+    t.cancel();
+    this.clock.tick(50);
+    t.restart();
+    this.clock.tick(50);
+    expect(handler).to.not.have.been.called;
+    this.clock.tick(50);
+    expect(handler).to.have.been.calledOnce;
   });
 });
